f0162782a868c8544f9d726917d25316ba5bf926
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-08-31 07:37:16 -0500

    S2.2 - DTO + Exceptions

--- Sem 1/src/main/java/com/medic/exception/ExceptionResponse.java
+++ Sem 1/src/main/java/com/medic/exception/ExceptionResponse.java
@@ -1,19 +1,18 @@
 package com.medic.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
+@Data // igual a @Setter @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ExceptionResponse {
-    private LocalDateTime date;
     private String message;
     private String details;
-
+    private HttpStatus status;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private LocalDateTime date;
 }
--- Sem 1/src/main/java/com/medic/exception/ResponseExceptionHandler.java
+++ Sem 1/src/main/java/com/medic/exception/ResponseExceptionHandler.java
@@ -4,6 +4,7 @@ package com.medic.exception;
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
+import com.medic.util.ExceptionMessageEnum;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestController
 public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
+    /*@ExceptionHandler(Exception.class)
     public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request){
         ExceptionResponse er = new ExceptionResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<ExceptionResponse>(er, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -45,5 +46,25 @@ public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
         ExceptionResponse er = new ExceptionResponse(LocalDateTime.now(), ex.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<ExceptionResponse>(er, HttpStatus.NOT_FOUND);
+    }*/
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAllExceptions(Exception exception, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(ExceptionMessageEnum.INCORRECT_REQUEST.getValues(), request.getDescription(false), HttpStatus.INTERNAL_SERVER_ERROR, LocalDateTime.now());
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
+
+    @ExceptionHandler(IncorrectRequestException.class)
+    public ResponseEntity<Object> handleIncorrectRequest(IncorrectRequestException exception, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(exception.getMessage(), request.getDescription(false), HttpStatus.BAD_REQUEST, LocalDateTime.now());
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
+
+    @ExceptionHandler(ModelNotFoundException.class)
+    public ResponseEntity<Object> handleResourceNotFound(ModelNotFoundException exception, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(exception.getMessage(), request.getDescription(false), HttpStatus.NOT_FOUND, LocalDateTime.now());
+        return new ResponseEntity<>(response, response.getStatus());
     }
 }
