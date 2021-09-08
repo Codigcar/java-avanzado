4268b031fabc9f9dd5cbfce3725df468b9ea9b6c
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-09-07 23:17:05 -0500

    pre-pc1 - Post y GetAll Profile

--- pre-pc1/src/main/java/com/upc/edu/pe/u201621893/controller/ProfileController.java
+++ pre-pc1/src/main/java/com/upc/edu/pe/u201621893/controller/ProfileController.java
@@ -1,5 +1,6 @@
 package com.upc.edu.pe.u201621893.controller;
 
+import com.upc.edu.pe.u201621893.dto.ProfileRequest;
 import com.upc.edu.pe.u201621893.dto.response.ProfileResponse;
 import com.upc.edu.pe.u201621893.model.Profile;
 import com.upc.edu.pe.u201621893.service.ProfileService;
@@ -7,11 +8,9 @@ import com.upc.edu.pe.u201621893.util.ProfileConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -29,5 +28,9 @@ public class ProfileController {
         List<Profile> profileList = profileService.getAll();
         return new ResponseEntity<>(profileConverter.convertProfileToDto(profileList), HttpStatus.OK);
     }
-
+    @PostMapping()
+    public ResponseEntity<Profile> createProfile( @Valid @RequestBody ProfileRequest profileRequest) throws Exception{
+        Profile profileNew = profileService.create(profileConverter.convertProfileToEntity(profileRequest));
+        return new ResponseEntity<>(profileNew, HttpStatus.CREATED);
+    }
 }
